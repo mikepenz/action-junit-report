b817730c181aa7d3cc13769a9027e081f364eb71
Mike Penz <mikepenz@gmail.com>
2021-12-17 14:34:01 +0100

    - fix lint error

--- src/testParser.ts
+++ src/testParser.ts
@@ -277,7 +277,7 @@ async function parseSuite(
           annotation_level: success ? 'notice' : 'failure',
           title: escapeEmoji(title),
           message: escapeEmoji(message),
-          raw_details: escapeEmoji(stackTrace),
+          raw_details: escapeEmoji(stackTrace)
         })
       }
     }
@@ -320,6 +320,7 @@ export async function parseTestReports(
  * Escape emoji sequences.
  */
 export function escapeEmoji(input: string): string {
-  const regex = /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/ug;
-  return input.replace(regex, match => ``); // replace emoji with empty string (\\u${(match.codePointAt(0) || "").toString(16)})
+  const regex =
+    /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/gu
+  return input.replace(regex, ``) // replace emoji with empty string (\\u${(match.codePointAt(0) || "").toString(16)})
 }
